e4d8ae387e990f32f9fb77c86ce60b6a0afcb541
James_McG13 <jamesmcgowanwork@hotmail.com>
2023-08-20 21:43:54 +0100

    Updated webpage to include portfolio projects

---
+++ src/Data/Portfolio.js
@@ -0,0 +1,19 @@
+export default [{
+    title: 'CMP Third Year Project',
+    imgUrl: 'src/assets/diss_screenshot.png',
+    stack: ['Python', 'Sklearn', 'Pandas', 'NumPy', 'Matplotlib'],
+    link: ''
+},
+{
+    title: 'Developing an Intelligent Chatbot: Train ticket finder',
+    imgUrl: 'src/assets/AI_screenshot.png',
+    stack: ['Python', 'Flask', 'JavaScript', 'NLTK', 'Sklearn'],
+    link: ''
+},
+{
+    title: 'Portfolio page',
+    imgUrl: '',
+    stack: ['React', 'Tailwind', 'vite'],
+    link: '',
+}
+]
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Typed from "react-typed";
 
 function Intro(){
     return (
         <div className="flex items-center justify center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">James McGowan</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer, Web developer, Data Scientist</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">Enter bio here</p>
+            <h1 className="text-4xl md:text-6xl mb-3 md:mb-3 font-bold">James McGowan</h1>
+            <Typed className="text-base md:text-3xl mb-6 font-medium"
+                strings={["Computer Science Graduate", "Software Engineer", "Data Scientist", "Web Developer", "Data Analyst"]}
+                typeSpeed={120}
+                backspeed={100}
+                loop
+            />
+            <p className="text-sm max-w-screen-xl mb-10 font-bold md:text-1xl">
+                👨‍💻 I'm a passionate computer science graduate from UEA, eager for embark on a tech career. With a solid foundation and a thirst for knowledge, I'm ready to explore diverse career paths armed with a first-class degree.
+                🚀 As a perpetual learner, I stay updated with industry trends and expand my toolkit. Problem-solving is my drive, and I thrive in collaborative environments, valuing diverse perspectives.
+                🎓 UEA gave me theory; now, I'm eager for hands-on industry experience, making an impact through software, algorithms, and open-source. 
+                💡 I'm open to various paths – from creation to analysis – thanks to the ever-evolving tech world. My goal is to contribute, learn, and embrace growth in the tech world!
+            </p>
         </div>
     )
 }
