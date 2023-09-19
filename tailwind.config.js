e2d1959bbe7c231308feb9c9a7854ef71ec2da50
James_McG13 <jamesmcgowanwork@hotmail.com>
2023-09-19 18:56:33 +0100

    Fixed images not displaying

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'code': "url('assets/cool-background.jpg')",
-        'UEA': "url('assets/UEA-background.jpg')",
-        'norwich': "url('assets/Norwich.png')",
-        'bury': "url('assets/Bury.png')",
-        'bury_2': "url('assets/Bury_2.jpg')",
+        'code': "url('/assets/cool-background.jpg')",
+        'UEA': "url('/assets/UEA-background.jpg')",
+        'norwich': "url('/assets/Norwich.png')",
+        'bury': "url('/assets/Bury.png')",
+        'bury_2': "url('/assets/Bury_2.jpg')",
       },
       fontFamily: {
         inter: ['inter', 'serif'],
