bcd9f3badf6f387e5fed776b9d91ae1f68843097
James_McG13 <jamesmcgowanwork@hotmail.com>
2023-10-06 19:36:11 +0100

    added scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, useRef} from "react"
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
 import Timeline from "./components/Timeline";
 import Intro from "./components/Intro";
 
+const RevealOnScroll = ({ children }) => { 
+  const [isVisible, setIsVisible] = useState(false); 
+  const ref = useRef(null); 
+
+  useEffect(() => { 
+      const scrollObserver = new IntersectionObserver(([entry]) => { 
+          if (entry.isIntersecting) { 
+              setIsVisible(true); 
+              scrollObserver.unobserve(entry.target); 
+          } 
+      }); 
+
+      scrollObserver.observe(ref.current); 
+
+      return () => { 
+          if (ref.current) { 
+              scrollObserver.unobserve(ref.current); 
+          } 
+      }; 
+  }, []); 
+
+  const classes = `transition-opacity duration-1000  
+      ${isVisible ? "opacity-100" : "opacity-0"
+      }`; 
+
+  return ( 
+      <div ref={ref} className={classes}> 
+          {children} 
+      </div> 
+  ); 
+}; 
 
 function App() {
     const [theme, setTheme] = useState(null);
@@ -74,21 +105,35 @@ function App() {
       </button>
       <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-serif">
         <div className="max-w-7xl w-11/12 mx-auto">
+          <RevealOnScroll>
           <Intro />
+          </RevealOnScroll>
           </div>
+          <RevealOnScroll>
           <picture className="flex md:max-w-none ps-0 pe-0 items-center justify-center md:h-96 mb-20 bg-fixed bg-cover bg-center md:bg-code"/>
+          </RevealOnScroll>
           <div className="max-w-7xl w-11/12 mx-auto">
+          <RevealOnScroll>
           <Portfolio />
+          </RevealOnScroll>
           </div>
+          <RevealOnScroll>
           <picture class="flex max-w-none ps-0 pe-0 items-center justify-center md:h-96 mb-20 bg-fixed bg-center bg-cover md:bg-UEA"/>
+          </RevealOnScroll>
+          <RevealOnScroll>
           <div className="max-w-5xl w-11/12 mx-auto">
           <Timeline />
           </div>
+          </RevealOnScroll>
+          <RevealOnScroll>
           <picture class="flex max-w-none ps-0 pe-0 items-center justify-center md:h-96 mb-20 bg-fixed bg-center bg-cover md:bg-bury_2"/>
+          </RevealOnScroll>
+          <RevealOnScroll>
           <div className="max-w-5xl w-11/12 mx-auto">
           <Contact />
           <Footer />
         </div>
+          </RevealOnScroll>
       </div>
      </>
     )
